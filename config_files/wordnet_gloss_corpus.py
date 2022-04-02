2c3b54fe041aff341d599b01ef698002467d9702
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2022-04-02 21:20:03 +0900

    ci: defined SREF basic lemma embeddings dataset.

--- config_files/wordnet_gloss_corpus.py
+++ config_files/wordnet_gloss_corpus.py
@@ -2,7 +2,6 @@
 # -*- coding:utf-8 -*-
 
 import os
-from dataset.filter import EmptyFilter, DictionaryFilter
 
 DIR_SREF_CORPUS = "/home/sakae/Windows/dataset/word_sense_disambiguation/SREF/corpus/"
 DIR_SREF_EMBEDDINGS = "/home/sakae/Windows/dataset/word_sense_disambiguation/SREF/vectors/"
@@ -12,9 +11,20 @@ cfg_gloss_corpus = {
     "SREF_Sense_Corpus": {
         "target_pos": ["n","v","s","r"],
         "concat_extended_examples": True,
-        "lemma_lowercase": True,
+        "lemma_surface_form_lowercase": False,
         "convert_adjective_to_adjective_satellite": True,
         "lst_path_extended_examples_corpus": [os.path.join(DIR_SREF_CORPUS, f"sentence_dict_{pos}") for pos in "n,v,a,r".split(",")],
         "description": "Word sense corpus used in SREF[Wang and Wang, EMNLP2020]. This corpus utilized a) WordNet lemmas, b) WordNet definition and examples, and c) examples collected by SREF authors."
+    }
+}
+
+cfg_embeddings = {
+    "SREF_basic_lemma_embeddings": {
+        "path": os.path.join(DIR_SREF_EMBEDDINGS, "emb_glosses_aug_gloss+examples.txt"),
+        "target_pos": ["n","v","s","r"],
+        "l2_norm": False,
+        "use_first_embeddings_only": True,
+        "lemma_surface_form_lowercase": False,
+        "description": "Basic lemma embeddings used in SREF[Wang and Wang, EMNLP2020]. This embeddings are computed using SREF Sense Corpus."
     }
 }
