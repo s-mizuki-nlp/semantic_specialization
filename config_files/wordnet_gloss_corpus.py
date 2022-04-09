e6263bf686848fbfc46f13a19efdfd30bd67e4ac
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2022-04-09 21:38:41 +0900

    ci: defined original wordnet gloss corpus

--- config_files/wordnet_gloss_corpus.py
+++ config_files/wordnet_gloss_corpus.py
@@ -15,6 +15,14 @@ cfg_gloss_corpus = {
         "convert_adjective_to_adjective_satellite": True,
         "lst_path_extended_examples_corpus": [os.path.join(DIR_SREF_CORPUS, f"sentence_dict_{pos}") for pos in "n,v,a,r".split(",")],
         "description": "Word sense corpus used in SREF[Wang and Wang, EMNLP2020]. This corpus utilized a) WordNet lemmas, b) WordNet definition and examples, and c) examples collected by SREF authors."
+    },
+    "WordNet_Gloss_Corpus": {
+        "target_pos": ["n","v","s","r"],
+        "concat_extended_examples": False,
+        "lemma_surface_form_lowercase": False,
+        "convert_adjective_to_adjective_satellite": True,
+        "lst_path_extended_examples_corpus": None,
+        "description": "WordNet Gloss corpus sense corpus used in SREF[Wang and Wang, EMNLP2020]. This corpus utilized a) WordNet lemmas and b) WordNet definition and examples."
     }
 }
 
