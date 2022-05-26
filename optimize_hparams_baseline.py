7fc3ab241acb2c2f918612b2a9ba1ca908851bb6
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2022-05-26 19:56:29 +0900

    refactor: adjust val. interval.

--- optimize_hparams_baseline.py
+++ optimize_hparams_baseline.py
@@ -44,12 +44,13 @@ def objective(trial: optuna.Trial):
     similarity_class_name = "CosineSimilarity"
 
     # optimization
-    dict_args["batch_size"] = trial.suggest_categorical("batch_size", [64,128,256,512])
-    if dict_args["batch_size"] == 512:
-        dict_args["val_check_interval"] = 400
-        dict_args["max_epochs"] = 20
-    elif dict_args["batch_size"] == 256:
+    batch_size = trial.suggest_categorical("batch_size", [64,128,256,512])
+    dict_args["val_check_interval"] = int(1000 * 128 / dict_args["batch_size"])
+    if batch_size == 256:
         dict_args["max_epochs"] = 15
+    elif batch_size == 512:
+        dict_args["max_epochs"] = 20
+    dict_args["batch_size"] = batch_size
 
     # contrastive task に関する条件付け
     cfg_contrastive_learning_dataset = {
