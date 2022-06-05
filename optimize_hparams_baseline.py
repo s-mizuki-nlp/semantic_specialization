9a8cfdf2974323b12b0962d5be6e5cccb537cd9d
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2022-06-05 19:10:19 +0900

    feat: further tuned hyper-param search range.

--- optimize_hparams_baseline.py
+++ optimize_hparams_baseline.py
@@ -45,7 +45,7 @@ def objective(trial: optuna.Trial):
     similarity_class_name = "CosineSimilarity"
 
     # optimization
-    batch_size = trial.suggest_categorical("batch_size", [64,128,256,512])
+    batch_size = trial.suggest_categorical("batch_size", [32,64,128,256])
     dict_args["val_check_interval"] = int(1000 * 128 / batch_size)
     if batch_size == 256:
         dict_args["max_epochs"] = 15
@@ -56,7 +56,7 @@ def objective(trial: optuna.Trial):
     # contrastive task に関する条件付け
     cfg_contrastive_learning_dataset = {
         "semantic_relation_for_positives": "all-relations",
-        "use_taxonomy_distance_for_sampling_positives": trial.suggest_categorical("use_taxonomy_distance_for_sampling_positives", [True,False]),
+        "use_taxonomy_distance_for_sampling_positives": False,
         "num_hard_negatives": trial.suggest_categorical("num_hard_negatives", [-1, 0, 1, 3, 5, 7, 9]) # 負例に用いる同形異義語の数．-1:無制限，0:なし，N(>0):N個まで
     }
     dict_args["use_positives_as_in_batch_negatives"] = trial.suggest_categorical("use_positives_as_in_batch_negatives", [True, False])
@@ -67,8 +67,8 @@ def objective(trial: optuna.Trial):
             return 0.0
 
     # max-pool margin task に関する条件付け．
-    dict_args["coef_max_pool_margin_loss"] = trial.suggest_loguniform("coef_max_pool_margin_loss", low=0.001, high=1.0)
-    max_margin = trial.suggest_discrete_uniform("max_margin", low=0.3, high=0.9, q=0.1)
+    dict_args["coef_max_pool_margin_loss"] = trial.suggest_loguniform("coef_max_pool_margin_loss", low=0.01, high=5.0)
+    max_margin = trial.suggest_discrete_uniform("max_margin", low=0.5, high=1.0, q=0.1)
     top_k = trial.suggest_int("top_k", low=1, high=7)
     dict_args["cfg_max_pool_margin_loss"] = {"max_margin": max_margin, "top_k": top_k}
 
@@ -80,11 +80,17 @@ def objective(trial: optuna.Trial):
 
     # gloss projection head configuration
     cfg_gloss_projection_head = {}
-    cfg_gloss_projection_head["n_layer"] = trial.suggest_int("n_layer", low=1, high=4, step=1)
+    cfg_gloss_projection_head["n_layer"] = 2
     if gloss_projection_head_name == "NormRestrictedShift":
-        cfg_gloss_projection_head["max_l2_norm_ratio"] = trial.suggest_loguniform("max_l2_norm_ratio", low=0.001, high=0.3)
+        cfg_gloss_projection_head["max_l2_norm_ratio"] = trial.suggest_loguniform("max_l2_norm_ratio", low=0.001, high=0.1)
         cfg_gloss_projection_head["init_zeroes"] = True
+        cfg_gloss_projection_head["distinguish_gloss_context_embeddings"] = trial.suggest_categorical("distinguish_gloss_context_embeddings", [True, False])
     dict_args["cfg_gloss_projection_head"] = cfg_gloss_projection_head
+
+    # distinguish_gloss_context_embeddings is effective for "SHARED" setting.
+    if context_projection_head_name == "Identity":
+        if cfg_gloss_projection_head["distinguish_gloss_context_embeddings"] == True:
+            return 0.0
 
     # context projection head configuration
     dict_args["cfg_context_projection_head"] = {}
