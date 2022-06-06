4d00a461f18828ee8ebaccf7c737a32ccc11809f
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2022-06-06 21:29:42 +0900

    ci: configured wiki40b corpora."

--- config_files/monosemous_corpus.py
+++ config_files/monosemous_corpus.py
@@ -21,53 +21,23 @@ cfg_corpus = {
         "path_stats": os.path.join(DIR_MONO_CORPUS, "wikitext103_train_pos-all_stats.jsonl"),
         "binary": False,
         "description": "Wikitext103 trainset > PoS tagging, lemmatization + MWE extraction > monosemous lemma sense annotation.",
-    }
+    },
+    "monosemous_wiki40b_all": {
+        "path": os.path.join(DIR_MONO_CORPUS, "wiki40b_train_all_paragraph_pos-all.jsonl"),
+        "path_stats": os.path.join(DIR_MONO_CORPUS, "wiki40b_train_all_paragraph_pos-all_stats.jsonl"),
+        "binary": False,
+        "description": "Wiki40b whole dataset > PoS tagging, lemmatization + MWE extraction > monosemous lemma sense annotation.",
+    },
 }
 
 cfg_embeddings = {
-    "wikitext103-subset": {
+    "monosemous_wikitext103_1-10": {
         "path":pick_first_available_path(
-            os.path.join(DIR_LOCAL, "bert-large-cased_wikitext103_train_freq=10-11_len=6-128.hdf5"),
-            os.path.join(DIR_EMBEDDINGS, "bert-large-cased_wikitext103_train_freq=10-11_len=6-128.hdf5"),
+            os.path.join(DIR_LOCAL, "bert-large-cased_monosemous_wikitext103_freq=1-10_len=6-128_random=True.hdf5"),
+            os.path.join(DIR_EMBEDDINGS, "bert-large-cased_monosemous_wikitext103_freq=1-10_len=6-128_random=True.hdf5"),
         ),
         "padding": False,
         "max_sequence_length": None,
-        "description": "BERT-large-cased. subset of the Wikitext-103 trainset, freq=10~11, length=6~128."
-    },
-    "wiki40b-first-paragraph-bert-base": {
-        "path":os.path.join(DIR_EMBEDDINGS, "bert-base-cased_wiki40b-train-first-paragraph_freq=10-100_len=6-128.hdf5"),
-        "padding": False,
-        "max_sequence_length": None,
-        "description": "BERT-base-cased. Wiki40b trainset first paragraph, freq=10~100, length=6~128."
-    },
-    "wiki40b-all": {
-        "path":os.path.join(DIR_EMBEDDINGS, "bert-large-cased_wiki40b-train-all-paragraph_freq=10-100_len=6-128.hdf5"),
-        "padding": False,
-        "max_sequence_length": None,
-        "description": "BERT-large-cased. Wiki40b trainset, freq=10~100, length=6~128."
-    },
-    "wiki40b-all-wide-vocab": {
-        "path":pick_first_available_path(
-            os.path.join(DIR_LOCAL, "bert-large-cased_wiki40b-train-all-paragraph_freq=5-200_len=6-128.hdf5"),
-            os.path.join(DIR_EMBEDDINGS, "bert-large-cased_wiki40b-train-all-paragraph_freq=5-200_len=6-128.hdf5"),
-        ),
-        "padding": False,
-        "max_sequence_length": None,
-        "description": "BERT-large-cased. Wiki40b trainset, unbiased, freq=5~200, length=6~128."
-    },
-    "wiki40b-all-narrow-vocab": {
-        "path":pick_first_available_path(
-            os.path.join(DIR_LOCAL, "bert-large-cased_wiki40b-train-all-paragraph_freq=100-300_len=6-128.hdf5"),
-            os.path.join(DIR_EMBEDDINGS, "bert-large-cased_wiki40b-train-all-paragraph_freq=100-300_len=6-128.hdf5"),
-        ),
-        "padding": False,
-        "max_sequence_length": None,
-        "description": "BERT-large-cased. Wiki40b trainset, freq=100~300, length=6~128."
-    },
-    "wiki40b-all-ext": {
-        "path":os.path.join(DIR_EMBEDDINGS, "bert-large-cased_wiki40b-train-all-paragraph_freq=100-200_len=6-128_random=False.hdf5"),
-        "padding": False,
-        "max_sequence_length": None,
-        "description": "BERT-large-cased. Wiki40b trainset, freq=100~200, length=6~128, disable random sampling."
-    },
+        "description": "BERT-large-cased. Wikitext-103 trainset, min_freq=1, max_sample=10, length=6~128."
+    }
 }
