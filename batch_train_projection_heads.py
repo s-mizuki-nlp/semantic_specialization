800ce21ca7ae52f13d1debcecb1d00cc66e4a502
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2022-07-11 19:34:07 +0900

    feat: added optional arguments; useful for ablation study.

--- batch_train_projection_heads.py
+++ batch_train_projection_heads.py
@@ -14,12 +14,18 @@ from train_projection_heads import main
 def nullable_string(value):
     return None if not value else value
 
+def nullable_json_loads(value):
+    value = value.replace("'","\"") if isinstance(value, str) else value
+    return {} if not value else json.loads(value)
+
+
 parser = argparse.ArgumentParser(description="batch trainer for {gloss,context} projection heads using BERT embeddings.")
 parser.add_argument("--path_args", "-a", required=True, type=str, help="path to the argument json file.")
 parser.add_argument("--repeats", "-r", required=False, type=int, default=1, help="number of repetition of the experiment.")
 parser.add_argument("--env_name", type=str, required=True, default=None, help="name parameter of TensorBoardLogger(). It can be handy for distinguishing experiment groups")
 parser.add_argument("--save_eval_metrics", required=False, type=nullable_string, default=None, help="save evaluation metrics to specified path with json format. if exists, appended.")
 parser.add_argument("--gpus", type=str, required=False, default=None, help="GPU ID used for optuna worker.")
+parser.add_argument("--optional_args", type=nullable_json_loads, required=False, default=None, help="optional arguments that is fed to train_projection_heads.py e.g., `{'coef_max_pool_margin_loss':1.0}`")
 parser.add_argument("--verbose", action="store_true", help="output verbosity.")
 args = parser.parse_args()
 
@@ -29,6 +35,9 @@ with io.open(args.path_args) as ifs:
 
 for arg_name in "env_name,gpus".split(","):
     dict_args_main[arg_name] = getattr(args, arg_name)
+
+for arg_name, arg_value in args.optional_args.items():
+    dict_args_main[arg_name] = arg_value
 
 for idx in range(args.repeats):
     main(dict_external_args=dict_args_main, returned_metric="hp/wsd_eval_ALL", verbose=args.verbose)
