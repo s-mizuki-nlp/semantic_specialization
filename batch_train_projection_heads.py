c272bd3598bf2ea6fe41420743f73b07eac80023
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2022-07-11 19:38:03 +0900

    fix: fixed argument name.

--- batch_train_projection_heads.py
+++ batch_train_projection_heads.py
@@ -22,7 +22,7 @@ def nullable_json_loads(value):
 parser = argparse.ArgumentParser(description="batch trainer for {gloss,context} projection heads using BERT embeddings.")
 parser.add_argument("--path_args", "-a", required=True, type=str, help="path to the argument json file.")
 parser.add_argument("--repeats", "-r", required=False, type=int, default=1, help="number of repetition of the experiment.")
-parser.add_argument("--env_name", type=str, required=True, default=None, help="name parameter of TensorBoardLogger(). It can be handy for distinguishing experiment groups")
+parser.add_argument("--name", type=str, required=True, default=None, help="name parameter of TensorBoardLogger(). It can be handy for distinguishing experiment groups")
 parser.add_argument("--save_eval_metrics", required=False, type=nullable_string, default=None, help="save evaluation metrics to specified path with json format. if exists, appended.")
 parser.add_argument("--gpus", type=str, required=False, default=None, help="GPU ID used for optuna worker.")
 parser.add_argument("--optional_args", type=nullable_json_loads, required=False, default=None, help="optional arguments that is fed to train_projection_heads.py e.g., `{'coef_max_pool_margin_loss':1.0}`")
@@ -33,7 +33,7 @@ assert os.path.exists(args.path_args), f"argument json file not found: {args.pat
 with io.open(args.path_args) as ifs:
     dict_args_main = json.load(ifs)
 
-for arg_name in "env_name,gpus".split(","):
+for arg_name in "name,gpus".split(","):
     dict_args_main[arg_name] = getattr(args, arg_name)
 
 for arg_name, arg_value in args.optional_args.items():
