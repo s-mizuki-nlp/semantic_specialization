39e685bcbed61434ed4c16301c40154fa941d02d
Sakae Mizuki <37898123+s-mizuki-nlp@users.noreply.github.com>
2023-05-15 16:49:47 +0900

    Update train_projection_heads.sh
    Updated usage example.

--- train_projection_heads.sh
+++ train_projection_heads.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 python train_projection_heads.py \
---gloss_dataset_name="SREF_basic_lemma_embeddings" \
---context_dataset_name="" \
---coef_max_pool_margin_loss=1.0 \
+--gloss_dataset_name="SREF_basic_lemma_embeddings_without_augmentation" \
+--context_dataset_name="SemCor-bert-large-cased" \
+--coef_max_pool_margin_loss=0.2 \
 --gloss_projection_head_name="NormRestrictedShift" \
---context_projection_head_name="SHARED" \
---cfg_contrastive_learning_dataset='{"num_hard_negatives":0}' \
+--context_projection_head_name="COPY" \
+--cfg_contrastive_learning_dataset='{"num_hard_negatives":5}' \
 --use_positives_as_in_batch_negatives=True \
 --gpus="" \
 --batch_size=256 \
---max_epochs=10
+--max_epochs=15
